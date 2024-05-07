5a90e068ad9dcce137cac3d0d76761564d934fa8
B20DCCN471-Trần Minh Nghĩa <88278748+MinhNghiaTran01@users.noreply.github.com>
2024-05-07 09:28:22 +0700

    ver1.0

--- clothes_service/clothes_search/views.py
+++ clothes_service/clothes_search/views.py
@@ -8,24 +8,14 @@ class ClothesSearch(APIView):
         query_params = request.query_params
         clothes = Clothes.objects.all()
         name = query_params.get('name')
-<<<<<<< HEAD
-        categoryId = query_params.get('categoryId')
-        # Thực hiện tìm kiếm dựa trên các tham số query
-        if name:
-            clothes = clothes.filter(name__icontains=name)
-        if categoryId:
-            clothes = clothes.filter(category_id=categoryId)
-=======
+
         category = query_params.get('category')
         categoryId = query_params.get('categoryId')
       
         # Thực hiện tìm kiếm dựa trên các tham số query
         if name:
             clothes = clothes.filter(name__icontains=name)
-        if category:
-            clothes = clothes.filter(category__name__icontains=category)
         if categoryId:
             clothes=clothes.filter(category_id=categoryId)
->>>>>>> e5a026fe711d6a603f7e4c36f8e4f070bca2bec1
         serializer = ClothesSearchSerializer(clothes, many=True)
         return Response(serializer.data)
